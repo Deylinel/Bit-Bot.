0a821815d9748ef16275061856913b4c07af0c39
Deylinel <deylibaquedano40@gmail.com>
2024-12-22 16:37:38 -0600

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -7,28 +7,32 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🧑‍💻 YA ESTAS REGISTRADO.\n\n*¿QUIERES HACERLO DE NUEVO?*\n\nUSE ESTE COMANDO PARA ELIMINAR SU REGISTRO.\n*${usedPrefix}unreg* <Número de serie>`)
-  if (!Reg.test(text)) return m.reply(`🤖 FORMATO INCORRECTO.\n\nUSO DEL COMANDO: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
+  if (user.registered === true) return m.reply(`🤖 ACCESO AUTORIZADO.\n\n*¿QUIERES REINICIAR TU REGISTRO?*\n\nUTILIZA EL COMANDO PARA ELIMINAR TU PERFIL.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`⚠️ FORMATO NO VÁLIDO.\n\nUSO DEL COMANDO: *${usedPrefix + command} nombre.edad*\nEjemplo: *${usedPrefix + command} ${name2}.16*`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('👻 El NOMBRE NO PUEDE ESTAR VACÍO.')
-  if (!age) return m.reply('👻 LA EDAD NO PUEDE ESTAR VACÍA.')
-  if (name.length >= 100) return m.reply('🫥 El NOMBRE ESTA MUY LARGO.' )
+  if (!name) return m.reply('💡 EL NOMBRE ES UN DATO OBLIGATORIO.')
+  if (!age) return m.reply('💡 LA EDAD ES UN DATO OBLIGATORIO.')
+  if (name.length >= 100) return m.reply('⚡ EL NOMBRE EXCEDE EL LÍMITE DE CARACTERES.')
   age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 WOW EL ABUELO QUIERE JUGAR AL BOT.')
-  if (age < 5) return m.reply('🚼 EL BEBE QUIERE JUGAR JAJA. ')
+  if (age > 100) return m.reply('👴🏻 ¡ERROR DE EDAD! ¿SOY UN PROTOTIPO ANTIGUO?')
+  if (age < 5) return m.reply('👶🏼 ALERTA: ¿ESTÁS EN LA FASE INICIAL DE LA MATRIX?')
+
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
+
   let sn = createHash('md5').update(m.sender).digest('hex')
   let img = await (await fetch(`https://i.ibb.co/Lg4nv9H/file.jpg`)).buffer()
-  let txt = ` –  *R E G I S T R O  -  BIT - BOT*\n\n`
-      txt += `┌  ❄️  *NOMBRE* : ${name}\n`
-      txt += `│  ❄️  *EDAD* : ${age} años\n`
-      txt += `│  ❄️  *NUMERO DE SERIE*\n`
-      txt += `└  ❄️  ${sn}`
-await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
-await m.react('✅')
+
+  let txt = `–  *REGISTRO DE DATOS - BOT PROTOCOL*\n\n`
+      txt += `┌  💻  *NOMBRE* : ${name}\n`
+      txt += `│  💻  *EDAD* : ${age} ciclos\n`
+      txt += `│  💻  *CÓDIGO DE IDENTIFICACIÓN*\n`
+      txt += `└  💻  ${sn}`
+
+  await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
+  await m.react('✅')
 }
 handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
 handler.tags = ['rg']
