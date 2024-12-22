588b5d6d445256594bb92af6d6441e8b849de9a7
Deylinel <deylibaquedano40@gmail.com>
2024-12-21 21:48:05 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://i.ibb.co/Lg4nv9H/file.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a unirte a los grupos oficiales de de CrowBot-Ai para convivir con la comunidad :D*
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de de BIT - BOT - AI para convivir con la comunidad :D*
 
 *✰* https://chat.whatsapp.com/HvyqEIGfMOL4h5EOkzbRzC
 
