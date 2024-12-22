f23d8c1de463cf55f2207fc3c8ea36ed9f7b6e48
Deylinel <deylibaquedano40@gmail.com>
2024-12-22 17:21:43 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -4,15 +4,13 @@ let handler = async (m, { conn, isAdmin, isROwner }) => {
     // Activación del bot con diseño futurista
     global.db.data.chats[m.chat].isBanned = false
     const futuristaMsg = `
-⚡️🔊 **BIT-BOT ACTIVADO EN ESTE GRUPO** 🔊⚡️
-
-💬 *El bot ha sido reactivado con éxito, y está listo para volver a funcionar en este chat.* 💬
+⚡️🔊 **BIT-BOT ACTIVADO** 🔊⚡️
 
 🚀 *Estado:* ACTIVADO 🚀
 
-🔑 *Si deseas hacer modificaciones adicionales, contacta con los administradores del grupo.* 🔑
+🔑 *Reactivar: Contactar admin* 🔑
 
-🛸 *¡Que la conectividad esté siempre contigo!* 🛸
+🛸 *¡Conectando de nuevo!* 🛸
     `
     await conn.reply(m.chat, futuristaMsg, m, rcanal)
     await m.react('✅')
