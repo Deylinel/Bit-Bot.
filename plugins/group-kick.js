a93911526a45f6105edf3b4bd54523e4591296b4
Deylinel <deylibaquedano40@gmail.com>
2024-12-21 20:44:22 -0600

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -6,8 +6,8 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`🚩 Usuario eliminado.`)
-m.reply(`Lo siento, acabas de ser eliminado del grupo.`, user)
+m.reply(`🚩 Usuario eliminado por pendejo.`)
+m.reply(`Lo siento, acabas de ser eliminado del grupo por pendejo.`, user)
 }
 
 handler.help = ['kick *@user*']
