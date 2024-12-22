e817d36c6b58fa6894eba3744c56742c2dde5491
Deylinel <deylibaquedano40@gmail.com>
2024-12-22 15:14:01 -0600

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -39,43 +39,47 @@ var handler = async (m, { conn }) => {
     let userNationality = userNationalityData ? `${userNationalityData.name} ${userNationalityData.emoji}` : 'Desconocido';
 
     let noprem = `
-「 👤 *PERFIL DE USUARIO* 」
-☁️ *Nombre:* ${username}
-💠 *Edad:* ${age}
-⚧️ *Genero:* ${genre}
-🎂 *Cumpleaños:* ${birth} 
-👩‍❤️‍👩 *Casad@:* ${isMarried ? partnerName : 'Nadie'}
-📜 *Descripción:* ${description}
-🌀 *Registrado:* ${registered ? '✅': '❌'}
-🌐 *Pais:* ${userNationality}
+╭─〘 👤 *USER PROFILE* 👤 〙─╮
+💬 *Name:* ${username}  
+🛠️ *Age:* ${age}  
+⚙️ *Gender:* ${genre}  
+🎉 *Birthday:* ${birth}  
+💍 *Marital Status:* ${isMarried ? partnerName : 'Single'}  
+📖 *Description:* ${description}  
+🔒 *Registered:* ${registered ? '✅' : '❌'}  
+🌍 *Country:* ${userNationality}  
 
-「 💰 *RECURSOS* 」
-💴 *Estrellas:* ${poin || 0}
-✨ *Experiencia:* ${exp || 0}
-⚜️ *Rango:* ${role}
-👑 *Premium:* ${premium ? '✅': '❌'}
+🔷 **RESOURCES** 🔷  
+🌌 *Stars:* ${estrellas || 0}  
+✨ *Experience:* ${exp || 0}  
+🪐 *Rank:* ${role}  
+🚀 *Premium:* ${premium ? '✅' : '❌'}  
+╰────────────────────╯`.trim();
+
+    let prem = `
+🛸 **── USER PREMIUM ──**  
+═══════════════════════  
+🔷 **USER INFORMATION** 🔷  
+📡 *Username:* ${username}  
+🛠️ *Age:* ${age}  
+⚙️ *Gender:* ${genre}  
+🎂 *Birthday:* ${birth}  
+💍 *Marital Status:* ${isMarried ? partnerName : 'Single'}  
+📖 *Description:* ${description}  
+🔒 *Registered:* ${registered ? '✅' : '❌'}  
+🌍 *Country:* ${userNationality}  
+
+═══════════════════════  
+🌟 **EXCLUSIVE FEATURES** 🌟  
+🛸 *Stars:* ${estrellas || 0}  
+💫 *Experience:* ${exp || 0}  
+⚜️ *Rank:* ${role}  
+═══════════════════════  
+🚀 *Outstanding User of the Future* 🚀  
 `.trim();
 
-    let prem = `╭──⪩ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ⪨
-│⧼👤⧽ *ᴜsᴜᴀʀɪᴏ:* *${username}*
-│⧼💠⧽ *ᴇᴅᴀᴅ:* *${age}*
-│⧼⚧️⧽ *ɢᴇɴᴇʀᴏ:* *${genre}*
-│⧼🎂⧽ *ᴄᴜᴍᴘʟᴇᴀɴ̃ᴏs:* ${birth}
-│⧼👩‍❤️‍👩⧽ *ᴄᴀsᴀᴅᴏ:* ${isMarried ? partnerName : 'Nadie'}
-📜 *ᴅᴇsᴄʀɪᴘᴄɪᴏɴ:* ${description}
-│⧼🌀⧽ *ʀᴇɢɪsᴛʀᴀᴅᴏ:* ${registered ? '✅': '❌'}
-│⧼🌐⧽ *ᴘᴀɪs:* ${userNationality}
-
-╰─────────────────⪨
-
-╭────⪩ 𝐑𝐄𝐂𝐔𝐑𝐒𝐎𝐒 ⪨
-│⧼💴⧽ *estrellas:* ${poin || 0}
-│⧼✨⧽ *ᴇxᴘᴇʀɪᴇɴᴄɪᴀ:* ${exp || 0}
-│⧼⚜️⧽ *ʀᴀɴɢᴏ:* ${role}
-╰───⪨ *𝓤𝓼𝓾𝓪𝓻𝓲𝓸 𝓓𝓮𝓼𝓽𝓪𝓬𝓪𝓭𝓸* ⪩`.trim();
-
-    conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, { mentions: [who] });
-}
+    conn.sendFile(m.chat, pp, 'profile.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, { mentions: [who] });
+};
 
 handler.help = ['profile'];
 handler.register = true;
