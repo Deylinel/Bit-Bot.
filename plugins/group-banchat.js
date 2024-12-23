72e4f0dcac2d88c5894a98accf68993d44ccec9d
Deylinel <deylibaquedano40@gmail.com>
2024-12-22 23:16:26 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,25 +1,11 @@
-let handler = async (m, { conn, isAdmin, isROwner }) => {
-    // Verificar si el usuario es administrador o el dueño del bot
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+let handler = async (m) => {
 
-    // Desactivar bot con diseño futurista
-    global.db.data.chats[m.chat].isBanned = true
-    const futuristaMsg = `
-🛸💻 **BIT-BOT DESACTIVADO** 💻🛸
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `✅ *BIT-BOT Ha Sido Desactivada En Este Chat*`, m, rcanal)
 
-🔒 *Acceso restringido* 🔒
-
-🚀 *Por:* ${m.pushName} 🚀
-
-⚡️ *Reactivar: Contactar admin* ⚡️
-    
-🎛️🔊 *¡Nos vemos pronto!* 🔊🎛️
-    `
-    await conn.reply(m.chat, futuristaMsg, m, rcanal)
-    await m.react('✅')
 }
-handler.help = ['banearbot']
-handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
-handler.group = true 
+handler.help = ['banchat']
+handler.tags = ['mods']
+handler.command = ['banchat']
+handler.rowner = true
 export default handler
