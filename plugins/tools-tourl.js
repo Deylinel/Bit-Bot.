d6de5291af1a4d8f7982dc2d0b918e865dc16b31
Deylinel <deylibaquedano40@gmail.com>
2024-12-22 19:16:35 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,41 +1,31 @@
 import fs from 'fs'
 import FormData from 'form-data'
 import axios from 'axios'
-import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
 
-  let q = m.quoted ? m.quoted : m
+  let q = m.quoted || m
   let mime = (q.msg || q).mimetype || ''
-
   await m.react('🕒')
-  if (!mime.startsWith('image/')) {
-    return m.reply('Por favor, responde a una *Imagen* para procesarla.')
-  }
+
+  if (!mime.startsWith('image/')) return m.reply('Responde con una *Imagen* 📸.')
 
   let media = await q.download()
   let formData = new FormData()
   formData.append('image', media, { filename: 'file' })
 
   let api = await axios.post('https://api.imgbb.com/1/upload?key=10604ee79e478b08aba6de5005e6c798', formData, {
-    headers: {
-      ...formData.getHeaders()
-    }
+    headers: { ...formData.getHeaders() }
   })
 
   await m.react('✅')
   if (api.data.data) {
-    let txt = '`🔧 IBB - Image Uploader API`\n\n'
-        txt += `*💻 TÍTULO* : ${q.filename || 'Desconocido'}\n`
-        txt += `*🔑 ID* : ${api.data.data.id}\n`
-        txt += `*🌐 ENLACE* : ${api.data.data.url}\n`
-        txt += `*🔗 ENLACE DIRECTO* : ${api.data.data.url_viewer}\n`
-        txt += `*🖥️ MIME* : ${mime}\n`
-        txt += `*📂 NOMBRE DE ARCHIVO* : ${q.filename || 'imagen.jpg'}\n`
-        txt += `*🖱️ EXTENSIÓN* : ${api.data.data.image.extension}\n`
-        txt += `*🗑️ ELIMINAR* : ${api.data.data.delete_url}\n\n`
-        txt += `*🔗 Desarrollado por: ${botname} - Soluciones Digitales*`
-    await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m, null, fake)
+    let txt = `🔧 *IBB - Image Uploader*\n\n`
+        txt += `*💾 ID* : ${api.data.data.id}\n`
+        txt += `*🌍 ENLACE* : ${api.data.data.url}\n`
+        txt += `*🔗 DIRECTO* : ${api.data.data.url_viewer}\n`
+        txt += `*🗑️ ELIMINAR* : ${api.data.data.delete_url}\n`
+    await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m)
   } else {
     await m.react('❌')
   }
