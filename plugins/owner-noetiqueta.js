28823c96a3e25aba764c0fc2f8ebbbb3b9261d47
Deylinel <deylibaquedano40@gmail.com>
2024-12-23 00:10:40 -0600

    Actualizar owner-noetiqueta.js

--- plugins/owner-noetiqueta.js
+++ plugins/owner-noetiqueta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
 await m.react('💛')
 await conn.reply(m.chat, `Hola @${m.sender.split`@`[0]} si necesitas la ayuda de mi creador porfavor escribele al privado\n*- Solo asuntos importantes -*`, estilo, { mentions: [m.sender] })
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おWILLZEK;;\nFN:おWILLZEK\nORG:おWILLZEK\nTITLE:\nitem1.TEL;waid=50557865603:50557865603\nitem1.X-ABLabel:おWILLZEK\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おWILLZEK\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おWILLZEK;;\nFN:おWILLZEK\nORG:おWILLZEK\nTITLE:\nitem1.TEL;waid=50488198573:50433222264\nitem1.X-ABLabel:おWILLZEK\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おWILLZEK\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'おWILLZEK', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.customPrefix = /^(@5218261275256|@5218132588591|@5218139760662|@5215659171599)$/i
